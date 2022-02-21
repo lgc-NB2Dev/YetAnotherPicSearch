d8d7540c716dd6ccf1fa17f03067afb9f9c62733
NekoAria <990879119@qq.com>
2022-02-21 08:08:58 +0800

    :art: 改进结构和代码格式

--- YetAnotherPicSearch/ascii2d.py
+++ YetAnotherPicSearch/ascii2d.py
@@ -23,7 +23,12 @@ async def ascii2d_search(url: str, proxy: str, hide_img: bool) -> list[str]:
             ]
             return "\n".join([i for i in res_list if i != ""])
 
-        color_final_res = "Ascii2D 色合検索\n" + await get_final_res(color_res)
-        bovw_final_res = "Ascii2D 特徴検索\n" + await get_final_res(bovw_res)
+        color_final_res = await get_final_res(color_res)
+        bovw_final_res = await get_final_res(bovw_res)
+        if color_final_res == bovw_final_res:
+            return [f"Ascii2D 色合検索与特徴検索結果完全一致\n{color_final_res}"]
 
-        return [color_final_res, bovw_final_res]
+        return [
+            f"Ascii2D 色合検索結果\n{color_final_res}",
+            f"Ascii2D 特徴検索結果\n{bovw_final_res}",
+        ]
