df2b65f88186cd9ac31c09c113765fc1e736b516
Neko Aria <990879119@qq.com>
2022-09-27 22:51:48 +0800

    feat(ascii2d): 重构搜索结果的格式，返回结果改为最多各 3 条

--- YetAnotherPicSearch/ascii2d.py
+++ YetAnotherPicSearch/ascii2d.py
@@ -10,47 +10,47 @@ from .utils import DEFAULT_HEADERS, get_image_bytes_by_url, handle_img, shorten_
 
 async def ascii2d_search(url: str, client: ClientSession, hide_img: bool) -> List[str]:
     ascii2d_color = Ascii2D(client=client)
-    color_res = await ascii2d_color.search(url)
+    file = await get_image_bytes_by_url(url)
+    color_res = await ascii2d_color.search(file=file)
     if not color_res.raw:
-        # 当 url 搜索抽风时，自动改用 file 搜索
-        file = await get_image_bytes_by_url(url)
-        color_res = await ascii2d_color.search(file=file)
-        if not color_res.raw:
-            return ["Ascii2D 暂时无法使用"]
-    # 针对 QQ 图片链接反盗链做处理
-    if color_res.raw[0].hash == "5bb9bec07e71ef10a1a47521d396811d":
-        url = f"https://images.weserv.nl/?url={url}"
-        color_res = await ascii2d_color.search(url)
+        return ["Ascii2D 暂时无法使用"]
     async with ClientSession(headers=DEFAULT_HEADERS) as session:
         resp = await session.get(
             color_res.url.replace("/color/", "/bovw/"), proxy=config.proxy
         )
         bovw_res = Ascii2DResponse(await resp.text(), str(resp.url))
 
-    async def get_final_res(res: Ascii2DResponse) -> List[str]:
-        selected_res = [i for i in res.raw if i.title or i.url][0]
-        thumbnail = await handle_img(selected_res.thumbnail, hide_img)
-        _url = await shorten_url(selected_res.url) if selected_res.url else ""
-        author = selected_res.author
-        if author and selected_res.author_url:
-            author_url = await shorten_url(selected_res.author_url)
-            author = f"[{author}]({author_url})"
-        res_list = [
-            thumbnail,
-            selected_res.title,
-            selected_res.detail,
-            f"作者：{author}" if author else "",
-            f"来源：{_url}" if _url else "",
-            f"搜索页面：{res.url}",
-        ]
-        return [i for i in res_list if i]
+    async def get_final_res(res: Ascii2DResponse, bovw: bool = False) -> List[str]:
+        final_res_list: List[str] = []
+        for r in res.raw:
+            if len(final_res_list) == 3:
+                break
+            if not (r.title or r.url_list):
+                continue
+            thumbnail = await handle_img(r.thumbnail, hide_img)
+            if not hide_img and thumbnail.startswith("预览图链接"):
+                continue
+            source = ""
+            if r.url:
+                source = await shorten_url(r.url)
+            elif r.url_list:
+                source = r.url_list[0][0]
+            author = r.author
+            if author and r.author_url:
+                author_url = await shorten_url(r.author_url)
+                author = f"[{author}]({author_url})"
+            res_list = [
+                thumbnail,
+                r.detail,
+                r.title,
+                f"作者：{author}" if author else "",
+                f"来源：{source}" if source else "",
+            ]
+            final_res_list.append("\n".join([i for i in res_list if i]))
+        return (
+            [f"Ascii2D 特徴検索結果\n搜索页面：{res.url}"] + final_res_list
+            if bovw
+            else [f"Ascii2D 色合検索結果\n搜索页面：{res.url}"] + final_res_list
+        )
 
-    color_final_res = await get_final_res(color_res)
-    bovw_final_res = await get_final_res(bovw_res)
-    if color_final_res[1:-1] == bovw_final_res[1:-1]:
-        return ["Ascii2D 色合検索与特徴検索結果完全一致\n" + "\n".join(color_final_res)]
-
-    return [
-        f"Ascii2D 色合検索結果\n" + "\n".join(color_final_res),
-        f"Ascii2D 特徴検索結果\n" + "\n".join(bovw_final_res),
-    ]
+    return await get_final_res(color_res) + await get_final_res(bovw_res, True)
