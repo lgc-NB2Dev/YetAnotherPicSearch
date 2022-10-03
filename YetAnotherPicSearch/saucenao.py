3117ad3b5b0d99b2b86dff0c0178d33136464172
Neko Aria <990879119@qq.com>
2022-10-04 03:01:10 +0800

    refactor(saucenao): 重构对搜索结果的处理逻辑

--- YetAnotherPicSearch/saucenao.py
+++ YetAnotherPicSearch/saucenao.py
@@ -4,6 +4,7 @@ from typing import List
 
 from aiohttp import ClientSession
 from PicImageSearch import SauceNAO
+from yarl import URL
 
 from .ascii2d import ascii2d_search
 from .config import config
@@ -40,7 +41,6 @@ async def saucenao_search(
     final_res = []
     if res and res.raw:
         selected_res = res.raw[0]
-        ext_urls = selected_res.origin["data"].get("ext_urls")
         # 如果结果为 pixiv ，尝试找到原始投稿，避免返回盗图者的投稿
         if selected_res.index_id == saucenao_db["pixiv"]:
             pixiv_res_list = list(
@@ -57,29 +57,25 @@ async def saucenao_search(
                     key=lambda x: int(re.search(r"\d+", x.url).group()),  # type: ignore
                 )
         # 如果地址有多个，优先取 danbooru
-        elif ext_urls and len(ext_urls) > 1:
-            for i in ext_urls:
+        elif len(selected_res.ext_urls) > 1:
+            for i in selected_res.ext_urls:
                 if "danbooru" in i:
                     selected_res.url = i
         _hide_img = hide_img or selected_res.hidden
         if selected_res.similarity < config.saucenao_low_acc:
             _hide_img = _hide_img or config.hide_img_when_low_acc
         thumbnail = await handle_img(selected_res.thumbnail, _hide_img)
-        if _source := selected_res.origin["data"].get("source", ""):
-            source = await shorten_url(_source)
-        else:
-            source = await shorten_url(await get_source(selected_res.url))
-        # 如果结果为 doujin ，尝试返回日文标题而不是英文标题
-        if selected_res.index_id in saucenao_db["doujin"]:  # type: ignore
-            if title := (
-                selected_res.origin["data"].get("jp_name")
-                or selected_res.origin["data"].get("eng_name")
-            ):
-                selected_res.title = title
+        source = selected_res.source
+        if source and source == selected_res.title:
+            source = ""
+        if not source and selected_res.url:
+            source = await get_source(selected_res.url)
+        if source and URL(source).host:
+            source = await shorten_url(source)
         _url = await shorten_url(selected_res.url)
         author = selected_res.author
-        if author and selected_res.index_id == saucenao_db["pixiv"]:
-            author = f'[{author}](https://pixiv.net/u/{selected_res.origin["data"]["member_id"]})'
+        if author and selected_res.author_url:
+            author = f"[{author}]({await shorten_url(selected_res.author_url)})"
         res_list = [
             f"SauceNAO ({selected_res.similarity}%)",
             thumbnail,
