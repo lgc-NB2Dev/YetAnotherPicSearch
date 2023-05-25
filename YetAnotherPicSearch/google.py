312d20b75b52121f1d48a081394254c239b6a28b
Neko Aria <990879119@qq.com>
2023-05-26 00:32:45 +0800

    fix: 修正 Google 和 Yandex 搜索引擎

--- YetAnotherPicSearch/google.py
+++ YetAnotherPicSearch/google.py
@@ -4,13 +4,14 @@ from httpx import AsyncClient
 from PicImageSearch import Google
 from PicImageSearch.model import GoogleResponse
 
-from .utils import async_lock
+from .utils import async_lock, get_image_bytes_by_url, shorten_url
 
 
 @async_lock()
-async def google_search(file: bytes, client: AsyncClient) -> List[str]:
+async def google_search(url: str, client: AsyncClient) -> List[str]:
     google = Google(client=client)
-    if res := await google.search(file=file):
+    _file = await get_image_bytes_by_url(url)
+    if res := await google.search(file=_file):
         return await search_result_filter(res)
     return ["Google 暂时无法使用"]
 
@@ -18,12 +19,13 @@ async def google_search(file: bytes, client: AsyncClient) -> List[str]:
 async def search_result_filter(
     res: GoogleResponse,
 ) -> List[str]:
+    url = await shorten_url(res.url)
     if not res.raw:
-        return [f"Google 搜索结果为空\n搜索页面：{res.url}"]
+        return [f"Google 搜索结果为空\n搜索页面：{url}"]
 
     selected_res = next((i for i in res.raw if i.thumbnail), res.raw[0])
     if not selected_res.thumbnail:
-        return [f"Google 搜索结果为空\n搜索页面：{res.url}"]
+        return [f"Google 搜索结果为空\n搜索页面：{url}"]
 
     thumbnail = f"[CQ:image,file=base64://{selected_res.thumbnail.split(',', 1)[1]}]"
     res_list = [
@@ -31,6 +33,6 @@ async def search_result_filter(
         thumbnail,
         selected_res.title,
         f"来源：{selected_res.url}",
-        f"搜索页面：{res.url}",
+        f"搜索页面：{url}",
     ]
     return ["\n".join([i for i in res_list if i])]
--- YetAnotherPicSearch/yandex.py
+++ YetAnotherPicSearch/yandex.py
@@ -4,13 +4,14 @@ from httpx import AsyncClient
 from PicImageSearch import Yandex
 from PicImageSearch.model import YandexResponse
 
-from .utils import async_lock, handle_img
+from .utils import async_lock, get_image_bytes_by_url, handle_img, shorten_url
 
 
 @async_lock()
-async def yandex_search(file: bytes, client: AsyncClient) -> List[str]:
+async def yandex_search(url: str, client: AsyncClient) -> List[str]:
     yandex = Yandex(client=client)
-    if res := await yandex.search(file=file):
+    _file = await get_image_bytes_by_url(url)
+    if res := await yandex.search(file=_file):
         return await search_result_filter(res)
     return ["Yandex 暂时无法使用"]
 
@@ -18,8 +19,9 @@ async def yandex_search(file: bytes, client: AsyncClient) -> List[str]:
 async def search_result_filter(
     res: YandexResponse,
 ) -> List[str]:
+    url = await shorten_url(res.url)
     if not res.raw:
-        return [f"Yandex 搜索结果为空\n搜索页面：{res.url}"]
+        return [f"Yandex 搜索结果为空\n搜索页面：{url}"]
 
     thumbnail = await handle_img(res.raw[0].thumbnail)
     res_list = [
@@ -30,6 +32,6 @@ async def search_result_filter(
         res.raw[0].source,
         res.raw[0].content,
         f"来源：{res.raw[0].url}",
-        f"搜索页面：{res.url}",
+        f"搜索页面：{url}",
     ]
     return ["\n".join([i for i in res_list if i])]
