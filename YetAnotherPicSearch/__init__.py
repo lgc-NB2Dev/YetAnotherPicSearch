e3d67c74f45e8c192d811be7d275a3eb8f24ca12
student_2333 <lgc2333@126.com>
2024-08-05 00:21:00 +0800

    up

--- YetAnotherPicSearch/__init__.py
+++ YetAnotherPicSearch/__init__.py
@@ -5,7 +5,7 @@ from nonebot.plugin import PluginMetadata, inherit_supported_adapters, require
 require("nonebot_plugin_alconna")
 
 from . import __main__ as __main__
-from .config import ConfigModel
+from .config import ConfigModel, config
 from .data_source import load_search_func
 
 load_search_func()
@@ -14,7 +14,7 @@ __version__ = "2.0.0"
 __plugin_meta__ = PluginMetadata(
     name="YetAnotherPicSearch",
     description="基于 NoneBot2 及 PicImageSearch 的另一个 NoneBot 搜图插件",
-    usage="",
+    usage=f"使用指令 `{config.search_keyword} -h` 查看帮助",
     type="application",
     homepage="https://github.com/NekoAria/YetAnotherPicSearch",
     config=ConfigModel,
